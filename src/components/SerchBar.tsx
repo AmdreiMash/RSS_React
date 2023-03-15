7715d76a3b287828af7687d2bab8fdd2f386d3c2
Andrei Mashedo <andreimashedo@MacBook-Air-Andrei.local>
2023-03-15 16:49:37 +0300

    feat: add search bar

---
+++ src/components/SerchBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './PagesHeader';
+import { Input } from 'antd';
+
+const { Search } = Input;
+
+class SearchBar extends React.Component {
+  state = { value: localStorage.getItem('Seach Value') || '' };
+  componentWillUnmount() {
+    localStorage.setItem('Seach Value', this.state.value);
+  }
+  render() {
+    return (
+      <Search
+        placeholder="Search..."
+        allowClear
+        onSearch={(value: string) => value !== '' && console.log(value)}
+        onChange={(e) => this.setState({ value: e.target.value })}
+        style={{ flex: '1', alignSelf: 'center' }}
+        value={this.state.value}
+      />
+    );
+  }
+}
+
+export default SearchBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Content } from 'antd/es/layout/layout';
+import SearchBar from '../components/SerchBar';
+import { Col, Row } from 'antd';
 
+const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0', minWidth: '200px' };
 class HomePage extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <h1>Home</h1>
-        <section></section>
+        <Content style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '30px', width: '90%' }}>
+            <h1>Home</h1>
+            <SearchBar />
+          </div>
+          <div style={{ display: 'flex', gap: '20px', width: '80%' }}></div>
+        </Content>
       </>
     );
   }
