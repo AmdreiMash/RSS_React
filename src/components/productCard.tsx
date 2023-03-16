1d174525afa553bf7bd642f5ca2ff7ff24de7cd0
Andrei Mashedo <andreimashedo@MacBook-Air-Andrei.local>
2023-03-16 21:50:33 +0300

    feat: add category and raiting in product card

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -3,6 +3,8 @@ import { IPriduct } from '../Types/interfaces';
 import { Image } from 'antd';
 import styled from './styled';
 
+const rateColor = ['black', 'red', '#E35049', '#E5D751', '#74F272', '#00F509'];
+
 class ProductCard extends React.Component<{ data: IPriduct }> {
   constructor(props: { data: IPriduct }) {
     super(props);
@@ -11,7 +13,18 @@ class ProductCard extends React.Component<{ data: IPriduct }> {
     return (
       <div style={{ ...styled.productWrapper, flexDirection: 'column' }}>
         <Image src={this.props.data.image} width={'80%'} height={'80%'} />
-        <p style={styled.productText}>{this.props.data.title}</p>
+        <div style={styled.productText}>{this.props.data.title}</div>
+        <div style={styled.productText}>{this.props.data.category}</div>
+        <div
+          style={{
+            ...styled.productText,
+          }}
+        >
+          Raiting:
+          <span style={{ color: `${rateColor[Math.floor(this.props.data.rating.rate)]}` }}>
+            {` ${this.props.data.rating.rate}`}
+          </span>
+        </div>
       </div>
     );
   }
