a1c31da2f39af7b55f065aab647a3e6aaf8f6d2b
Andrei Mashedo <andreimashedo@MacBook-Air-Andrei.local>
2023-03-17 13:38:04 +0300

    feat: complate product cards

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -2,28 +2,51 @@ import React from 'react';
 import { IPriduct } from '../Types/interfaces';
 import { Image } from 'antd';
 import styled from './styled';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Like } from '../assets/like.svg';
 
 const rateColor = ['black', 'red', '#E35049', '#E5D751', '#74F272', '#00F509'];
 
 class ProductCard extends React.Component<{ data: IPriduct }> {
+  state: {
+    liked: boolean;
+  };
   constructor(props: { data: IPriduct }) {
     super(props);
+    this.state = {
+      liked: false,
+    };
   }
+
+  likeHandler = () => this.setState({ liked: !this.state.liked });
+
   render() {
     return (
       <div style={{ ...styled.productWrapper, flexDirection: 'column' }}>
-        <Image src={this.props.data.image} width={'80%'} height={'80%'} />
-        <div style={styled.productText}>{this.props.data.title}</div>
-        <div style={styled.productText}>{this.props.data.category}</div>
-        <div
-          style={{
-            ...styled.productText,
-          }}
-        >
-          Raiting:
-          <span style={{ color: `${rateColor[Math.floor(this.props.data.rating.rate)]}` }}>
-            {` ${this.props.data.rating.rate}`}
-          </span>
+        <Image src={this.props.data.image} width={'40%'} height={'60%'} />
+        <div style={styled.productPrice}>{`${this.props.data.price}$`}</div>
+        <div style={styled.productInfo}>
+          <div style={styled.productName}>{this.props.data.title}</div>
+          <div style={styled.productText}>
+            Category: <Link to="">{` ${this.props.data.category}`}</Link>
+          </div>
+          <div
+            style={{
+              ...styled.productText,
+            }}
+          >
+            Raiting:
+            <span style={{ color: `${rateColor[Math.floor(this.props.data.rating.rate)]}` }}>
+              {` ${this.props.data.rating.rate}`}
+            </span>
+          </div>
+          <Like
+            style={styled.like}
+            fill={this.state.liked ? '#FF7979' : '#808080'}
+            height={25}
+            width={25}
+            onClick={this.likeHandler}
+          />
         </div>
       </div>
     );
--- src/components/styled.ts
+++ src/components/styled.ts
@@ -1,6 +1,6 @@
 const styled: { [key: string]: React.CSSProperties } = {
   productWrapper: {
-    width: '200px',
+    width: '300px',
     height: '350px',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
@@ -8,11 +8,32 @@ const styled: { [key: string]: React.CSSProperties } = {
     border: '1px solid  rgba(0, 0, 0, 0.09)',
     borderRadius: '5px',
     display: 'flex',
-    //justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    paddingTop: '5px',
   },
-  productText: { backgroundColor: '#91A2AE', width: '100%' },
+  productText: { width: '100%', marginLeft: '8px' },
   SearchBar: { flex: '1', alignSelf: 'center' },
+  productName: {
+    marginLeft: '8px',
+    fontSize: '1.5em',
+    fontWeight: 'bold',
+    textOverflow: 'ellipsis',
+    maxHeight: '1.3em',
+    maxWidth: '100%',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+  },
+  productInfo: {
+    width: '100%',
+    position: 'relative',
+    paddingBottom: '10px',
+  },
+  like: {
+    position: 'absolute',
+    bottom: '5px',
+    right: '15px',
+  },
 };
 
 export default styled;
