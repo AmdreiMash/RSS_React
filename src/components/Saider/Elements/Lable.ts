e190cbf6d4b8fa8f06991abe9327b7d912d05ebc
Andrei Mashedo <ahdma@mail.ru>
2023-03-30 19:21:45 +0300

    refactor: rewrite name input

--- src/components/Saider/Elements/Lable.ts
+++ src/components/Saider/Elements/Lable.ts
@@ -1,12 +1,13 @@
+import { FieldError } from 'react-hook-form/dist/types';
 import styled from 'styled-components';
 
-const Label = styled.label<{ status: boolean; message: string; bottom?: string }>`
+const Label = styled.label<{ status: FieldError | undefined; message: string; bottom?: string }>`
   position: relative;
-  color: ${(props) => (props.status ? 'inherit' : 'red')};
+  color: ${(props) => (props.status ? 'red' : 'inherit')};
   display: flex;
   gap: 8px;
   &:after {
-    content: '${(props) => (props.status ? '' : props.message)}';
+    content: '${(props) => (props.status ? props.message : '')}';
     position: absolute;
     left: 0;
     bottom: ${(props) => (props.bottom ? props.bottom : '-12px')};
--- src/components/Saider/Form.tsx
+++ src/components/Saider/Form.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
 import styled from '../styled';
 import FormWrapper from './Elements/FormWrapper';
 import Input from './Elements/Input';
@@ -18,129 +18,129 @@ import { FormValue } from '../../Types/Types';
 import birthdayValidation from '../../utils/birthdayValidation';
 import resetValues from '../../utils/resetValues';
 import InputImg from './InputImg';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-class Form extends React.Component<{ addNewCard(data: FormValue): void }, FormState> {
-  constructor(props: { addNewCard(data: FormValue): void }) {
-    super(props);
+const Form = (props: { addNewCard(data: FormValue): void }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+    defaultValues: {
+      name: '',
+      email: '',
+      birthday: '',
+      country: 'Country',
+      gender: '',
+      policy: false,
+      male: false,
+      female: false,
+      file: '',
+    },
+  });
 
-    this.state = {
-      name: true,
-      email: true,
-      birthday: true,
-      country: true,
-      gender: true,
-      file: true,
-      policy: true,
-    };
-  }
+  //const getData = (data) => {
+  //  const name = nameValidation(data.name);
+  //  const email = emailValidation(data.email);
+  //  const birthday = birthdayValidation(data.birthday);
+  //  const country = getValue(data.country) === 'Country' ? false : getValue(this.country);
+  //  const img = getFile(data.file);
+  //  const file = img ? URL.createObjectURL(img) : false;
+  //  const policy = data.policy.current?.checked as boolean;
+  //  const gender = data.male.current?.checked
+  //    ? 'male'
+  //    : data.female.current?.checked
+  //    ? 'female'
+  //    : false;
 
-  name = React.createRef<HTMLInputElement>();
-  email = React.createRef<HTMLInputElement>();
-  birthday = React.createRef<HTMLInputElement>();
-  country = React.createRef<HTMLSelectElement>();
-  file = React.createRef<HTMLInputElement>();
-  policy = React.createRef<HTMLInputElement>();
-  male = React.createRef<HTMLInputElement>();
-  female = React.createRef<HTMLInputElement>();
+  //  const values: FormValue = { gender, name, email, birthday, country, file, policy };
+  //  return values;
+  //};
 
-  getData = () => {
-    const name = nameValidation(this.name);
-    const email = emailValidation(this.email);
-    const birthday = birthdayValidation(this.birthday);
-    const country = getValue(this.country) === 'Country' ? false : getValue(this.country);
-    const img = getFile(this.file);
-    const file = img ? URL.createObjectURL(img) : false;
-    const policy = this.policy.current?.checked as boolean;
-    const gender = this.male.current?.checked
-      ? 'male'
-      : this.female.current?.checked
-      ? 'female'
-      : false;
+  //const resetForm = () => {
+  //  resetValues([this.name, this.email, this.birthday, this.policy, this.male, this.female]);
+  //  this.country &&
+  //    this.country.current &&
+  //    this.country.current.value &&
+  //    (this.country.current.value = 'Country');
+  //  this.file &&
+  //    this.file.current &&
+  //    this.file.current.files &&
+  //    (this.file.current.files = new DataTransfer().files);
+  //};
 
-    const data: FormValue = { gender, name, email, birthday, country, file, policy };
-    return data;
-  };
+  //const submitHeader = (e: FormEvent) => {
+  //  const data: FormValue = this.getData();
+  //  const check: { [key: string]: boolean } = {};
 
-  resetForm = () => {
-    resetValues([this.name, this.email, this.birthday, this.policy, this.male, this.female]);
-    this.country &&
-      this.country.current &&
-      this.country.current.value &&
-      (this.country.current.value = 'Country');
-    this.file &&
-      this.file.current &&
-      this.file.current.files &&
-      (this.file.current.files = new DataTransfer().files);
-  };
+  //  for (const key in data) {
+  //    if (data[key] === false) {
+  //      check[key] = false;
+  //    } else {
+  //      check[key] = true;
+  //    }
+  //  }
 
-  submitHeader = (e: FormEvent) => {
-    const data: FormValue = this.getData();
-    const check: { [key: string]: boolean } = {};
+  //  e.preventDefault();
 
-    for (const key in data) {
-      if (data[key] === false) {
-        check[key] = false;
-      } else {
-        check[key] = true;
-      }
-    }
+  //  this.setState({ ...this.state, ...check });
 
-    e.preventDefault();
+  //  if (!Object.values(check).includes(false)) {
+  //    this.props.addNewCard(data);
+  //    this.resetForm();
+  //  }
+  //};
 
-    this.setState({ ...this.state, ...check });
-
-    if (!Object.values(check).includes(false)) {
-      this.props.addNewCard(data);
-      this.resetForm();
-    }
-  };
-
-  render() {
-    return (
-      <FormWrapper onSubmit={this.submitHeader}>
-        <Label status={this.state.name} message={errorMessage.name} bottom={'-25px'}>
-          Name
-          <Input
-            status={this.state.name} //! this attribute will simply change the input color if the value fails validation. I didn't have to do that) But it turns out prettier this way :) do not consider it an error, there is a similar functionality for input components in all popular libraries like Antd or MUI. This component remains uncontrolled anyway
-            ref={this.name}
-            name="name"
-          />
-        </Label>
-        <Label status={this.state.email} message={errorMessage.email} bottom={'-25px'}>
-          Email
-          <Input ref={this.email} name="email" status={this.state.email} />
-        </Label>
-        <Label status={this.state.birthday} message={errorMessage.birthday}>
-          Birthday
-          <input name="birthday" type="date" ref={this.birthday} />
-        </Label>
-        <Label status={this.state.country} message={errorMessage.country}>
-          Сountry
-          <Select link={this.country} />
-        </Label>
-        <Label status={this.state.gender} message={errorMessage.gender}>
-          Gender:
-          <Radio male={this.male} female={this.female} />
-        </Label>
-        <InputImg status={this.state.file} ref={this.file} />
-        {/*<LabelFile status={this.state.file} message={'Add your foto'} style={styled.label}>
+  return (
+    <FormWrapper onSubmit={handleSubmit((data) => console.log(data))}>
+      <Label status={errors.name} message={errorMessage.name} bottom="-30px">
+        Name
+        <Input
+          status={true}
+          {...register('name', {
+            required: true,
+            maxLength: 30,
+            pattern: /^[A-ZА-Я][a-zа-я]+/,
+          })}
+        />
+      </Label>
+      <Label status={erroe.email} message={errorMessage.email} bottom={'-25px'}>
+        Email
+        <Input ref={this.email} name="email" status={this.state.email} />
+      </Label>
+      {/*<Label status={this.state.birthday} message={errorMessage.birthday}>
+        Birthday
+        <input name="birthday" type="date" ref={this.birthday} />
+      </Label>*/}
+      {/*<Label status={this.state.country} message={errorMessage.country}>
+        Сountry
+        <Select link={this.country} />
+      </Label>*/}
+      {/*<Label status={this.state.gender} message={errorMessage.gender}>
+        Gender:
+        <Radio male={this.male} female={this.female} />
+      </Label>*/}
+      {/*<InputImg status={this.state.file} ref={this.file} />*/}
+      {/*<LabelFile status={this.state.file} message={'Add your foto'} style={styled.label}>
           Add foto
           <InputFile ref={this.file} type="file" name="file" accept="image/*" multiple={false} />
         </LabelFile>*/}
-        <Label
-          status={this.state.policy}
-          message={errorMessage.policy}
-          style={{ ...styled.label, fontSize: '0.8em' }}
-        >
-          <p>
-            I have read and agree with the <a href="#">privacy policy</a>
-          </p>
-          <input type="checkbox" ref={this.policy} />
-        </Label>
-        <SubmitButton />
-      </FormWrapper>
-    );
-  }
-}
+      {/*<Label
+        status={this.state.policy}
+        message={errorMessage.policy}
+        style={{ ...styled.label, fontSize: '0.8em' }}
+      >
+        <p>
+          I have read and agree with the <a href="#">privacy policy</a>
+        </p>
+        <input type="checkbox" ref={this.policy} />
+      </Label>*/}
+      <SubmitButton />
+    </FormWrapper>
+  );
+};
 
 export default Form;
