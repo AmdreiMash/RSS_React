5ef3318ddd9811ae14d2159923f636e879b1b29d
Andrei Mashedo <ahdma@mail.ru>
2023-05-01 02:46:40 +0300

    feat: add ctpress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import task from '@cypress/code-coverage/task';
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: 'cypress/**/*.*',
+      include: './***',
+    },
+  },
   e2e: {
+    baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      task(on, config);
+
+      return config;
     },
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import ssr from 'vite-plugin-ssr/plugin';
+import istanbul from 'vite-plugin-istanbul';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
@@ -22,6 +24,15 @@ export default defineConfig({
     ssr({
       prerender: true,
       includeAssetsImportedByServer: true,
+    }),
+
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+
+    dts({
+      insertTypesEntry: true,
     }),
   ],
   ssr: {
